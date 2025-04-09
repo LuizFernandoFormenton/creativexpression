7099914e5be54e16b0fd86a7295a34c9e46f6977
ArthurSilva175 <arthurskiinwalker16@gmail.com>
2025-04-08 21:52:49 -0300

    Usuarios

--- creativexpression/Usuario.cs
+++ creativexpression/Usuario.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Java.Sql;
+using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,15 @@ namespace creativexpression
 {
     internal class Usuario
     {
+        public int id { get; }
+        public string nome { get; set; }
+        public string data_nascimento { get; set; }
+        public int sexo { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+        public string telefone { get; set; }
+        public string cep { get; set; }
+
     }
 }
