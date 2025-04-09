32c5f901d824ce93963157a5950da6b2c7d57bba
Luiz Fernando <luizformenton14@gmail.com>
2025-04-08 21:47:00 -0300

    criação das classes

--- creativexpression/Produto.cs
+++ creativexpression/Produto.cs
@@ -8,5 +8,12 @@ namespace creativexpression
 {
     internal class Produto
     {
+
+        public int id {  get; set; }
+        public string nome_do_produto { get; set; }
+        public float preco {  get; set; }
+        public string tamanho {  get; set; }
+        
+
     }
 }
