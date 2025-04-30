607a873e70142da733cd199ad83acbfa90827c97
ArthurSilva175 <arthurskiinwalker16@gmail.com>
2025-04-29 21:00:57 -0300

    Página inicial ok

--- creativexpression/MainPage.xaml.cs
+++ creativexpression/MainPage.xaml.cs
@@ -1,15 +1,21 @@
-﻿namespace creativexpression
+﻿using System;
+using Microsoft.Maui.Controls;
+    
+namespace creativexpression
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-
         }
 
+        // Evento acionado ao clicar no botão "Entrar"
+        private async void OnEntrarClicked(object sender, EventArgs e)
+        {
+            // Exemplo de navegação para a página de listagem de usuários
+            await Navigation.PushAsync(new ListagemUsuarios());
+        }
     }
-
 }
+
